a7670396d4a008710ce7f44d2cd05371d2831e3f
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-04-10 20:04:24 +0600

    TGL-27-feat(request): Create GET request for get all PUP

--- routers/pup.ts
+++ routers/pup.ts
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
-import { Router } from 'express';
+import {Router} from 'express';
 
-// import auth, {RequestWithUser} from "../middleware/auth";
-// import permit from "../middleware/permit";
 import {PupTypes} from "../types";
 import Pup from "../models/Pup";
 import permit from "../middleware/permit";
+import PUP from "../models/Pup";
 import auth, {RequestWithUser} from "../middleware/auth";
-
 
 export const pupRouter = Router();
 
-pupRouter.post('/', auth, permit('admin'), async (req:RequestWithUser, res, next) => {
+pupRouter.post('/', auth, permit('admin'), async (req: RequestWithUser, res, next) => {
     try {
+        if(req.body.region === '' || req.body.settlement === '' || req.body.address === '') {
+            return res.status(422).send({message: 'Field is not to be ad empty!'})
+        }
         const pupData: PupTypes = {
             region: req.body.region,
             settlement: req.body.settlement,
@@ -27,6 +28,15 @@ pupRouter.post('/', auth, permit('admin'), async (req:RequestWithUser, res, next
         if (e instanceof mongoose.Error.ValidationError) {
             return res.status(422).send(e);
         }
+        next(e);
+    }
+});
+
+pupRouter.get('/', auth, permit('admin'), async (_req, res, next) => {
+    try {
+        const pup = await PUP.find();
+        return res.send(pup);
+    } catch (e) {
         next(e);
     }
 });
