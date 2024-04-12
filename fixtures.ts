53a303393bc472ee9ef791f4a91fe63e788351ab
Elhan Omurbekov <jeenbae1968@gmail.com>
2024-04-12 19:28:31 +0600

    TGL-15 add fixtures

--- fixtures.ts
+++ fixtures.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import config from './config';
 import User from "./models/User";
+import Pup from "./models/Pup";
 
 const dropCollection = async (
     db: mongoose.Connection,
@@ -24,6 +25,22 @@ const run = async () => {
             await dropCollection(db, collectionName);
         }
 
+        const pups = await Pup.create([
+            {
+                region: 'Чуйская',
+                settlement: 'г.Бишкек',
+                address: 'пр.Манаса 44'
+            }, {
+                region: 'Таласская',
+                settlement: 'г.Талас',
+                address: 'ул. Бердике Баатыра 191'
+            }, {
+                region: 'Иссык-Кульская',
+                settlement: 'г.Каракол',
+                address: 'ул.Ленина 186/1'
+            }
+        ]);
+
         await User.create([
             {
                 email: 'test_admin@gmail.com',
@@ -31,11 +48,13 @@ const run = async () => {
                 firstName: "Admin",
                 lastName: "Admin",
                 middleName: "Admin",
+                pupID: pups[0],
                 phoneNumber: 996709888111,
                 marketId: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000,
                 region: "Чуй",
                 token: crypto.randomUUID(),
                 role: 'admin',
+                address: "Чуйкова 122"
             },
             {
                 email: 'test_manager@gmail.com',
@@ -43,11 +62,13 @@ const run = async () => {
                 firstName: "Manager",
                 lastName: "Manager",
                 middleName: "Manager",
+                pupID: pups[1],
                 phoneNumber: 996709888111,
                 marketId: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000,
                 region: "Джалал-Абад",
                 token: crypto.randomUUID(),
                 role: 'manager',
+                address: "Чуйкова 122"
             },
             {
                 email: 'test_user-1@gmail.com',
@@ -55,10 +76,12 @@ const run = async () => {
                 firstName: "DefaultUser-1",
                 lastName: "DefaultUser-1",
                 middleName: "DefaultUser-1",
+                pupID: pups[2],
                 phoneNumber: 996709888111,
                 marketId: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000,
                 region: "Иссык-Куль",
                 token: crypto.randomUUID(),
+                address: "Чуйкова 122"
             },
             {
                 email: 'test_user-2@gmail.com',
@@ -66,10 +89,12 @@ const run = async () => {
                 firstName: "DefaultUser-2",
                 lastName: "DefaultUser-2",
                 middleName: "DefaultUser-2",
+                pupID: pups[0],
                 phoneNumber: 996709888111,
                 marketId: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000,
                 region: "Нарын",
                 token: crypto.randomUUID(),
+                address: "Чуйкова 122"
             },
             {
                 email: 'test_user-3@gmail.com',
@@ -77,10 +102,12 @@ const run = async () => {
                 firstName: "DefaultUser-3",
                 lastName: "DefaultUser-3",
                 middleName: "DefaultUser-3",
+                pupID: pups[0],
                 phoneNumber: 996709888111,
                 marketId: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000,
                 region: "Ош",
                 token: crypto.randomUUID(),
+                address: "Чуйкова 122"
             },
             {
                 email: 'test_user-4@gmail.com',
@@ -88,6 +115,7 @@ const run = async () => {
                 firstName: "DefaultUser-4",
                 lastName: "DefaultUser-4",
                 middleName: "DefaultUser-4",
+                pupID: pups[0],
                 phoneNumber: 996709888111,
                 marketId: Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000,
                 region: "Талас",
