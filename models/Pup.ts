42e2fee20f077deba29d421539a1d7c2609b81d0
elaman sargaldaev <melisbekovelaman311@gmail.com>
2024-04-13 18:22:07 +0600

    TGL-38 added eslint,Prettier for Backend

--- models/Pup.ts
+++ models/Pup.ts
@@ -1,34 +1,45 @@
-import {model, Schema} from "mongoose";
+import { model, Schema } from 'mongoose';
 
-const PUPSchema = new Schema({
+const PUPSchema = new Schema(
+  {
     name: {
       type: String,
       required: true,
     },
 
     region: {
-        type: String,
-        required: true,
-        enum: ['Чуйская', 'Иссык-Кульская', 'Таласская', 'Нарынская', 'Джалал-Абадская', 'Ошская', 'Баткенская'],
+      type: String,
+      required: true,
+      enum: [
+        'Чуйская',
+        'Иссык-Кульская',
+        'Таласская',
+        'Нарынская',
+        'Джалал-Абадская',
+        'Ошская',
+        'Баткенская',
+      ],
     },
 
     settlement: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
 
     address: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
 
     phoneNumber: Number,
 
     isChina: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-}, { versionKey: false });
+  },
+  { versionKey: false },
+);
 
 const PUP = model('PUP', PUPSchema);
 
--- routers/pup.ts
+++ routers/pup.ts
@@ -1,42 +1,42 @@
-import mongoose from "mongoose";
-import {Router} from 'express';
+import mongoose from 'mongoose';
+import { Router } from 'express';
 
-import {PupTypes} from "../types";
-import Pup from "../models/Pup";
-import permit from "../middleware/permit";
-import PUP from "../models/Pup";
-import auth, {RequestWithUser} from "../middleware/auth";
+import { PupTypes } from '../types';
+import Pup from '../models/Pup';
+import permit from '../middleware/permit';
+import PUP from '../models/Pup';
+import auth, { RequestWithUser } from '../middleware/auth';
 
 export const pupRouter = Router();
 
 pupRouter.post('/', auth, permit('admin'), async (req: RequestWithUser, res, next) => {
-    try {
-        if(req.body.region === '' || req.body.settlement === '' || req.body.address === '') {
-            return res.status(422).send({message: 'Field is not to be ad empty!'})
-        }
-        const pupData: PupTypes = {
-            region: req.body.region,
-            settlement: req.body.settlement,
-            address: req.body.address,
-        };
-
-        const pup = new Pup(pupData);
-        await pup.save();
-
-        return res.send({message: 'Pup is correctly added!', pup});
-    } catch (e) {
-        if (e instanceof mongoose.Error.ValidationError) {
-            return res.status(422).send(e);
-        }
-        next(e);
+  try {
+    if (req.body.region === '' || req.body.settlement === '' || req.body.address === '') {
+      return res.status(422).send({ message: 'Field is not to be ad empty!' });
     }
+    const pupData: PupTypes = {
+      region: req.body.region,
+      settlement: req.body.settlement,
+      address: req.body.address,
+    };
+
+    const pup = new Pup(pupData);
+    await pup.save();
+
+    return res.send({ message: 'Pup is correctly added!', pup });
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(e);
+    }
+    next(e);
+  }
 });
 
 pupRouter.get('/', auth, permit('admin'), async (_req, res, next) => {
-    try {
-        const pup = await PUP.find();
-        return res.send(pup);
-    } catch (e) {
-        next(e);
-    }
+  try {
+    const pup = await PUP.find();
+    return res.send(pup);
+  } catch (e) {
+    next(e);
+  }
 });
