df62478a95bee349d486df9e8281dff07e6c2398
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-04-15 17:54:59 +0600

    "upd1TGL-27-feat(add route): Add pups route to server.ts file"

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import config from './config';
 import usersRouter from './routers/users';
+import {pupRouter} from "./routers/pup";
 
 const app = express();
 const port = 8000;
@@ -11,6 +12,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 app.use('/users', usersRouter);
+app.use('/pups', pupRouter);
 
 const run = async () => {
   await mongoose.connect(config.mongoose.db);
