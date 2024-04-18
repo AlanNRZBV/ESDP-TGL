ebcc99e9d476f3d28f09a28fa848a02e4accae96
Elhan Omurbekov <jeenbae1968@gmail.com>
2024-04-18 21:04:11 +0600

    TGL-45 add delete route

--- routers/users.ts
+++ routers/users.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import User from '../models/User';
+import { Filter } from '../user.type';
+import auth, { RequestWithUser } from '../middleware/auth';
+import permit from '../middleware/permit';
 
 const usersRouter = express.Router();
 
@@ -31,9 +34,51 @@ usersRouter.post('/', async (req, res, next) => {
   }
 });
 
+usersRouter.post('/', auth, permit('super', 'admin'), async (req: RequestWithUser, res, next) => {
+  try {
+    const isSuperAdmin = req.user?.role === 'super';
+    const newUser = new User({
+      email: req.body.email,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      middleName: req.body.middleName,
+      pupID: req.body.pupID,
+      region: req.body.region,
+      phoneNumber: req.body.phoneNumber,
+      address: req.body.address,
+      settlement: req.body.settlement,
+      role: isSuperAdmin ? 'admin' : 'manager',
+    });
+
+    newUser.generateMarketID();
+    newUser.generateToken();
+    await newUser.save();
+    return res.send({ message: 'OK!', newUser });
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(e);
+    }
+    next(e);
+  }
+});
+
 usersRouter.get('/', async (req, res, next) => {
   try {
-    const users = await User.find();
+    const { region, settlement, role } = req.query;
+
+    const filter: Filter = {};
+
+    if (region) {
+      filter.region = region as string;
+    }
+    if (settlement) {
+      filter.settlement = settlement as string;
+    }
+    if (role) {
+      filter.role = role as string;
+    }
+    const users = await (Object.keys(filter).length > 0 ? User.find(filter) : User.find());
     res.send(users);
   } catch (e) {
     next(e);
@@ -105,5 +150,35 @@ usersRouter.delete('/sessions', async (req, res, next) => {
     return next(e);
   }
 });
+
+usersRouter.delete(
+  '/:id',
+  auth,
+  permit('admin', 'super'),
+  async (req: RequestWithUser, res, next) => {
+    try {
+      const role = req.user?.role;
+      const itemId = req.params.id;
+
+      const user = await User.findById(itemId);
+
+      if (!user) {
+        return res.status(404).send({ error: 'User not found!' });
+      }
+
+      if (user.role === 'client' && role === 'admin') {
+        await User.findByIdAndDelete(itemId);
+      } else if (role === 'super') {
+        await User.findByIdAndDelete(itemId);
+      } else {
+        return res.status(404).send({ error: 'You cannot delete this user!' });
+      }
+
+      res.send({ success: true, message: 'User deleted successfully.' });
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
 export default usersRouter;
