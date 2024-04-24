7e52d060d0e3d1dea5eb422a224e9939391edb36
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2024-04-24 12:34:36 +0600

    TGL-68.fix: fixed patch and delete requests in regions router, added message

--- routers/regions.ts
+++ routers/regions.ts
@@ -3,6 +3,7 @@ import Region from '../models/Region';
 import permit from '../middleware/permit';
 import auth from '../middleware/auth';
 import { RegionData } from '../types/regions.types';
+import mongoose from 'mongoose';
 
 const regionsRouter = Router();
 
@@ -10,9 +11,9 @@ regionsRouter.get('/', async (req, res, next) => {
   try {
     const regions = await Region.find();
     if (regions.length < 1) {
-      return res.status(404).send({ error: 'Не найдено ни одного региона. Попробуйте позже' });
+      return res.status(404).send({ message: 'Не найдено ни одного региона. Попробуйте позже' });
     }
-    return res.send(regions);
+    return res.send({ message: 'Список регионов', regions });
   } catch (e) {
     next(e);
   }
@@ -27,17 +28,26 @@ regionsRouter.post('/add', auth, permit('super'), async (req, res, next) => {
 
     const region = new Region(newRegion);
     await region.save();
+    return res.send({ message: `Регион ${region.name} успешно создан`, region });
   } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(e);
+    }
     next(e);
   }
 });
 
 regionsRouter.patch('/:id', auth, permit('super'), async (req, res, next) => {
   try {
-    const regionId = { id: req.params.id };
-    const updateData = { name: req.body.name };
+    const regionId = { _id: req.params.id };
+    const updateData: RegionData = { name: req.body.name, lang: req.body.lang };
     const updatedRegion = await Region.findOneAndUpdate(regionId, updateData, { new: true });
-    return res.send(updatedRegion);
+
+    if (!updatedRegion) {
+      return res.status(404).send({ message: 'Регион не найден' });
+    }
+
+    return res.send({ message: 'Данные успешно обновлены', updatedRegion });
   } catch (e) {
     next(e);
   }
@@ -45,9 +55,14 @@ regionsRouter.patch('/:id', auth, permit('super'), async (req, res, next) => {
 
 regionsRouter.delete('/:id', auth, permit('super'), async (req, res, next) => {
   try {
-    const regionId = { id: req.params.id };
+    const regionId = { _id: req.params.id };
     const deletedRegion = await Region.findOneAndDelete(regionId);
-    return res.send(deletedRegion);
+
+    if (!deletedRegion) {
+      return res.status(404).send({ message: 'Регион не найден' });
+    }
+
+    return res.send({ message: 'Регион успешно удален', deletedRegion });
   } catch (e) {
     next(e);
   }
